5c700b4f8b9942031634f90c1af27f1cf7dacb5c
unknown <parksw3@mcmaster.ca>
2018-10-16 18:45:32 -0400

    update

--- measles/simulate.R
+++ measles/simulate.R
@@ -1,55 +1,60 @@
-simfun <- function(fitted,
+simfun <- function(tmat, ## transmission matrix ncity by nperiod
+				   alpha,
+				   mixmat, ## m[i, j] represents j -> i
+				   I0, S0,
 				   nsim=10,
-				   method=c("deterministic", "poisson", "nbinom"),
+				   method=c("nbinom", "deterministic", "poisson"),
 				   popmat,
-				   birthmat) {
+				   birthmat, ## rhomat needs to be a column longer
+				   rhomat) { 
 	method <- match.arg(method)
 	
 	mfun <- switch(method,
-				   deterministic=function(x) x,
-				   poisson=function(x) rpois(length(x), x),
-				   nbinom=function(x) rnbinom(length(x), mu=x, size=x))
+				   deterministic=function(x, y) x,
+				   poisson=function(x, y) rpois(length(x), x),
+				   nbinom=function(x, y) rnbinom(length(x), mu=x, size=y))
 	
-	pp <- exp(predict(fitted$gamm4fit$gam,
-				  newdata=data.frame(
-				  	period=rep(1:52, ncol(fitted$distmat)),
-				  	logI=0,
-				  	off=0,
-				  	region=rep(colnames(fitted$Imat), each=52) 
-				  )))
+	rfun <- switch(method,
+				   function(x, p) rbinom(length(x), round(x), p),
+				   deterministic=function(x,p) x*p) 
 	
-	alpha <- fixef(fitted$gamm4fit$mer)[2] + ranef(fitted$gamm4fit$mer)$region$logI
-	
-	trans <- matrix(pp, nrow=52)
+	ncity <- nrow(tmat)
+	nperiod <- ncol(tmat)
 	
 	reslist <- vector('list', nsim)
 	
-	mixmat <- fitted$M
-	
 	for (i in 1:nsim) {
-		simmatI <- matrix(0, nrow=nrow(fitted$Imat), ncol=ncol(fitted$Imat))
-		simmatS <- matrix(0, nrow=nrow(fitted$Imat), ncol=ncol(fitted$Imat))
+		simmatI <- matrix(0, nrow=nrow(popmat), ncol=ncol(popmat)+1)
+		simmatC <- matrix(0, nrow=nrow(popmat), ncol=ncol(popmat)+1)
+		simmatS <- matrix(0, nrow=nrow(popmat), ncol=ncol(popmat)+1)
 			
-		simmatI[1,] <- fitted$Imat[1,]
-		simmatS[1,] <- fitted$Smat[1,]
+		simmatI[,1] <- I0
+		simmatS[,1] <- S0
+		simmatC[,1] <- rfun(I0, 1/rhomat[,1])
 		
-		for (t in 2:nrow(simmatI)) {
+		for (t in 2:ncol(simmatI)) {
 			period <- t %% 52
 			
 			if (period==0) period <- 52
 			
-			incidence <- mfun(trans[period,]*simmatS[t-1,]*(simmatI[t-1,] %*% mixmat)^alpha/popmat[t-1,])
+			Iprev <- mixmat %*% simmatI[,t-1]
 			
-			ii <- pmin(simmatS[t-1,], incidence)
+			if (all(Iprev==0)) break
 			
-			simmatI[t,] <- ii
-			simmatS[t,] <- simmatS[t-1,] - ii + birthmat[t-1,]
+			incidence <- mfun(tmat[,period]*simmatS[,t-1]*(Iprev)^alpha/popmat[,t-1], Iprev)
+			
+			ii <- pmin(simmatS[,t-1], incidence)
+			
+			simmatI[,t] <- ii
+			simmatS[,t] <- simmatS[,t-1] - ii + birthmat[,t-1]
+			simmatC[,t] <- rfun(ii, 1/rhomat[,t])
 		}
 		
 		reslist[[i]] <- list(
 			I=simmatI,
-			S=simmatS
+			S=simmatS,
+			C=simmatC
 		)
 	}
-	
+	reslist
 }
---
+++ measles/simulate_US.R
@@ -0,0 +1,116 @@
+library(rstan)
+library(dplyr)
+library(ggplot2); theme_set(theme_bw())
+library(gridExtra)
+source("fitfun.R")
+source("simulate.R")th
+
+load("stan_US.rda")
+
+measles_data <- read.csv("measlesUKUS.csv")
+
+measles_US <- measles_data %>% 
+	filter(country=="US") %>%
+	mutate(cases=ifelse(is.na(cases), 0, cases)) %>%
+	filter(year >= 1920, year <= 1940)
+
+measles_list <- measles_US %>%
+	split(as.character(.$loc))
+
+nn <- names(tail(sort(sapply(measles_list, function(x) max(x$pop))), 10))
+
+measles_list <- measles_list[nn] ## for teseting purposes
+
+reconstruct_list <- measles_list %>%
+	lapply(function(data) reconstruct(data$cases, data$rec))
+
+birthmat <- measles_list %>%
+	lapply("[[", "rec") %>%
+	do.call(what="cbind") %>%
+	head(-1) %>%
+	t
+
+rhomat <- reconstruct_list %>%
+	lapply("[[", "rho") %>%
+	do.call(what="cbind") %>%
+	t
+
+ext <- rstan::extract(fit)
+
+x <- 1:52; k <- seq(0, 52, by=2) + 0.5
+BX <- cSplineDes(x,k)
+
+psample <- seq(10, 1000, by=10)
+
+sumlist <- vector('list', 100)
+
+for (j in 1:100) {
+	i <- psample[j]
+	tmat <- exp(ext$amat[i,,] %*% t(BX))
+	
+	alpha <- ext$alpha[i,]
+	
+	mixmat <- ext$m[i,,]
+	
+	popmat <- standata$popmat
+	
+	I0 <- standata$Iprev[,1]
+	S0 <- round(ext$sbar[i,,1] + standata$Zmat[,1])
+	
+	set.seed(101)
+	sim <- simfun(tmat, alpha, mixmat, I0, S0, popmat=popmat, birthmat=birthmat, rhomat=rhomat)
+	
+	sumlist[[j]] <- lapply(sim, function(x){
+		cc <- x$C
+		
+		data.frame(
+			city=nn,
+			max=apply(cc, 1, max),
+			zero=apply(cc, 1, function(y) sum(y==0)/ncol(cc))
+		)
+	}) %>%
+		bind_rows(.id="sim")
+}
+
+summdf <- sumlist %>%
+	bind_rows(.id="param") %>%
+	gather(key, value, -city, -sim, -param) %>%
+	group_by(key, city) %>%
+	summarize(
+		mean=mean(value),
+		lwr=quantile(value, 0.025),
+		upr=quantile(value, 0.975)
+	)
+
+truesumm <- measles_list %>%
+	lapply(function(x){
+		data.frame(
+			city=unique(x$loc),
+			max=max(x$cases),
+			zero=sum(x==0)/nrow(x)
+		)
+	}) %>%
+	bind_rows(.id="city") %>%
+	gather(key, value, -city) %>%
+	rename(tvalue=value)
+
+combdf <- merge(summdf, truesumm) %>%
+	mutate(tvalue=as.numeric(as.character(tvalue)))
+
+g1 <- ggplot(combdf %>% filter(key=="max")) +
+	geom_point(aes(tvalue, mean)) +
+	geom_errorbar(aes(tvalue, ymin=lwr, ymax=upr), width=0.1) +
+	geom_abline(intercept=0, slope=1) +
+	xlab("True maxima") +
+	ylab("Simulated maxima")
+
+g2 <- ggplot(combdf %>% filter(key=="zero")) +
+	geom_point(aes(tvalue, mean)) +
+	geom_errorbar(aes(tvalue, ymin=lwr, ymax=upr), width=0.0005) +
+	geom_abline(intercept=0, slope=1) +
+	xlab("True proportion of zeroes") +
+	ylab("Simulated proportion of zeroes")
+
+gtot <- arrangeGrob(g1, g2, nrow=1)
+
+ggsave("simulate_US.pdf", gtot, width=8, height=6)
